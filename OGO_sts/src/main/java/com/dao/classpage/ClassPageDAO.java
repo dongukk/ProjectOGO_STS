b92cc27bb55ffa62b0f046534cdb858b532fece8
Eom Sohyeon <esh9797@gmail.com>
2022-09-27 17:59:37 +0900

    ClassPageController 구현중

--- OGO_sts/src/main/java/com/dao/classpage/ClassPageDAO.java
+++ OGO_sts/src/main/java/com/dao/classpage/ClassPageDAO.java
@@ -1,9 +1,30 @@
 package com.dao.classpage;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.dto.classpage.ClassDTO;
 
 @Repository
 public class ClassPageDAO {
+	@Autowired
+	SqlSessionTemplate template;
+	
+	public ClassDTO selectClass(int classNum) {
+		ClassDTO cDTO= template.selectOne("ClassMapper.selectClass", classNum);
+		return cDTO;
+	}
+
+	public String selectNickName(String tutorId) {
+		String nickName=template.selectOne("ClassMapper.selectNickName", tutorId);
+		return nickName;
+	}
+
+	public String userProfilePhoto(String tutorId) {
+		String tutorProfile=template.selectOne("ClassMapper.userProfilePhoto", tutorId);
+		return tutorProfile;
+	}
 	
 	
 }
