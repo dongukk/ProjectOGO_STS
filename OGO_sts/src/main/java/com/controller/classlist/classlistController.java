1f2adac7ed9ef8351e76e6c1a59a240f6a3ac3d8
Minje_Kim <109512808+kmj20021@users.noreply.github.com>
2022-10-13 22:50:10 +0900

    home 정렬

--- OGO_sts/src/main/java/com/controller/classlist/classlistController.java
+++ OGO_sts/src/main/java/com/controller/classlist/classlistController.java
@@ -16,22 +16,48 @@ public class classlistController {
 	@Autowired
 	classlistService service;
 
+	@SuppressWarnings("unused")
 	@RequestMapping(value = "/home2")//home2로 접근, 밑에 함수로 감
 	//@ResponseBody  //return 값으로 test
-	public String home2(String subcategory, Model m) { 
+	public String home2(String subcategory, Model m, String mesg) { 
 		
-		if (subcategory == null) {//subcategory가 널값일때 밑에 적용
+		String a = "desc";
+		List<classlistDTO> list = service.selectallnew(a);
+		System.out.println("new 와 처음"+list);
+		m.addAttribute("classlist", list);
+		
+		if (subcategory == null && mesg != null) {//subcategory가 널값일때 밑에 적용
+			System.out.println("카테고리 없을 때 : "+mesg);
 			
-			List<classlistDTO> list = service.selectall();//service-dao-mapper 데이터 가져오는 과정 list에 담겨있음
-			m.addAttribute("classlist", list);//classlist로 변수명, list의 데이터를 m이 들고 home2로 따라가서 ${}실행
-		
+			if (mesg.equals("new")) {
+				list = service.selectallnew(a);
+				System.out.println("new 와 처음"+list);
+				m.addAttribute("classlist", list);
+			}else if (mesg.equals("asc") ) {
+				list = service.selectallA();
+				System.out.println("low"+list);
+				m.addAttribute("classlist", list);
+			}else if(mesg.equals("desc") ) {
+				list = service.selectallD();
+				System.out.println("high"+list);
+				m.addAttribute("classlist", list);
+			}else {
+				System.out.println("여기 오면 안됨");
+			}
+			
+			/*
+			 * List<classlistDTO> list = service.selectall();//service-dao-mapper 데이터 가져오는
+			 * 과정 list에 담겨있음 m.addAttribute("classlist", list);//classlist로 변수명, list의 데이터를
+			 * m이 들고 home2로 따라가서 ${}실행
+			 */		
 		} else if (subcategory != null) {
-			System.out.println(subcategory);//카테고리를 잘 가져왔는지 확인
+			System.out.println("카테고리 있을 때 : "+subcategory);//카테고리를 잘 가져왔는지 확인
 			
-			List<classlistDTO> list = service.selectcate(subcategory);
+			 list = service.selectcate(subcategory);
 			m.addAttribute("classlist", list);
+			
 		}
-		
+		System.out.println("끝!!!!!");
 		return "home2";
 	}
 }
--- OGO_sts/src/main/java/com/service/classlist/classlistService.java
+++ OGO_sts/src/main/java/com/service/classlist/classlistService.java
@@ -13,17 +13,22 @@ public class classlistService {
 	@Autowired
 	classlistDAO dao;
 
-	public List<classlistDTO> selectall() {
-		List<classlistDTO> list=dao.selectall();
-		return list;
-	}
 
 	public List<classlistDTO> selectcate(String subcategory) { /*classlistDTO가 담긴 selectcate라는 변수명의 List를 반환할거임-String subcategory를 받아라!*/
-		
 		List<classlistDTO> list = dao.selectcate(subcategory);/*dao.안에 selectcate 함수를 실행시키면서 subcategory 가져가라!*/
 		
 		return list;
 	}
 
-	
+	public List<classlistDTO> selectallnew(String desc) {
+		return dao.selectallnew(desc);
+	}
+
+	public List<classlistDTO> selectallA() {
+		return dao.selectallA();
+	}
+
+	public List<classlistDTO> selectallD() {
+		return dao.selectallD();
+	}
 }
